eded49802498ec771faa8c9457dfada44101c84b
liraop <pedrolira@live.com>
2018-10-17 19:33:15 -0300

    melhorado logs

--- miniteste04.py
+++ miniteste04.py
@@ -10,47 +10,53 @@ status = ''
 
 def main():
     status = 'Livre'
-    ambiente.process(gera_cliente())
-    ambiente.process(recebe_cliente())
-    ambiente.process(termino_servico())
+    ambiente.process(gera_cliente(status))
+    #ambiente.process(recebe_clinte())
+    #ambiente.process(atende_cliente())
+    #ambiente.process(termina_servico())
 
     print('Simulação iniciada')
-    ambiente.run(until=200)
+    ambiente.run(until=100)
     print('Simulação completa')
 
-def gera_cliente():
-    PRIORIDADE_ALTA = 1
+def gera_cliente(status):
+    while True:
+        PRIORIDADE_ALTA = 1
 
-    cliente = (id, classe) =  'Cliente %s' % randint(1,1000), randint(1,2)
+        cliente = (id, classe) =  'Cliente %s' % randint(1,1000), randint(1,2)
 
-    proxima_entrada = 0
+        proxima_entrada = 0
 
-    if (cliente[1] == PRIORIDADE_ALTA):
-        print('Entrada cliente classe 1 as %.3f' % ambiente.now)
-        proxima_entrada = uniform(1,10)
-    else:
-        print('Entrada cliente classe 2 as %.3f' % ambiente.now)
-        proxima_entrada = uniform(1,5)
+        if (cliente[1] == PRIORIDADE_ALTA):
+            proxima_entrada = uniform(1,10)
+        else:
+            proxima_entrada = uniform(1,5)
 
-    yield ambiente.timeout(proxima_entrada)
-    print('Próxima entrada escalonada para daqui a %.2f ts' % proxima_entrada)
+        if (status == 'Livre'):
+            yield ambiente.process(atende_cliente(cliente))
+        else:
+            yield ambiente.process(enfileira_cliente(cliente))
 
-def recebe_cliente():
-    if (servico.count == 0 ):
-        print("Cliente sendo processado em %.3f" % ambiente.now)
-    else:
-        print("Cliente enviado para fila em %.3f" % ambiente.now)
-        with servico.request(cliente[0], priority=cliente[1]) as req:
-            yield req
+        yield ambiente.timeout(proxima_entrada)
+        print('[%.3f] Próxima entrada escalonada para daqui a %.2f ts' % (ambiente.now, proxima_entrada))
+
+def atende_cliente(cliente):
+    print('[%.3f] %s sendo atendido' % (ambiente.now, cliente[0]))
+    tempo_atendimento = uniform(5,10)
+    yield ambiente.timeout(tempo_atendimento)
+    print('[%.3f] %s atendido em %.2f tps' % (ambiente.now, cliente[0], tempo_atendimento))
+
+def enfileira_cliente(cliente):
+    print('[%.3f] Servidor ocupado. %s enfileirado.' % (ambiente.now, cliente[0]))
 
 
-def termino_servico():
+def termina_servico():
     if not servico.queue:
-        print("Servidor livre")
+        print('[%.3f] Servidor livre' % (ambiente.now))
     else:
-        print(servico.queue)
         yield ambiente.timeout(uniform(3,7))
-        servico.release()
+        yield ambiente.process(termina_servico())
+
 
 if __name__ == '__main__':
     main()
